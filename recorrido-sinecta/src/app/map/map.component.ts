c7beb5b7b4e2d73deb955118c0bd3a3743071c49
JorgeEstebanCardenas <A00232305@itesm.mx>
2022-11-11 11:47:16 -0700

    Añadir poligono del terreno

--- recorrido-sinecta/src/app/map/map.component.ts
+++ recorrido-sinecta/src/app/map/map.component.ts
@@ -25,19 +25,36 @@ L.Marker.prototype.options.icon = iconDefault;
 export class MapComponent implements AfterViewInit {
   private map:any;
 
+  
+
   constructor(private markerService: MarkerService) { }
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [ 29.170121381348366, -110.91119347821426 ],
+      zoom: 17
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
+      maxZoom: 24,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
+
+    const coords: any = [
+      [29.169078321915375, -110.91216967085387],
+      [29.169414793858298, -110.91102649789431],
+      [29.16937101839368, -110.90934167752444],
+      [29.17084718316487, -110.90929018812271],
+      [29.171356717345038, -110.91306607758207],
+      [29.170509990037182, -110.91343508496108]
+  
+    ];
+
+    const area = L.polygon(
+      coords, 
+      {color: 'blue', fillColor: 'blue', fillOpacity: 0.1}
+    ).addTo(this.map);
 
     tiles.addTo(this.map);
   }
