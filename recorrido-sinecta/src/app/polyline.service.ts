4468a2c3a90316da7527bf4b457b0be32569b8de
Silvana-Ruiz <silvanadorita@hotmail.com>
2022-11-16 22:08:45 -0700

    Path Styles and Flag markers

--- recorrido-sinecta/src/app/polyline.service.ts
+++ recorrido-sinecta/src/app/polyline.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,15 +19,60 @@ export class PolylineService {
       }
       
       console.log(coordinates);
-      
-      var polyline = L.polyline(coordinates, {color: 'blue'});
+
+      var polyline = L.polyline(coordinates, {color: 'black'});
       polyline.addTo(map);
-   
 
-
-    });
-    
-
+    });  
   }
+
+  makeStartIcon(map:any): void{
+    
+    this.http.get(this.path).subscribe((res: any) => {
+
+      console.log( "lt " + (res.features[0].geometry.coordinates.at(0)).at(1));
+      console.log(( "long " + res.features[0].geometry.coordinates.at(0)).at(0));
+      
+      const svgIcon = L.divIcon({
+        html: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-3" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00b341" fill="#00b341" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16" />
+</svg>`,
+        className: "",
+        iconSize: [24, 40],
+        iconAnchor: [12, 40],
+      }); 
+      
+      //L.marker( [29.171134011489812, -110.91209942941005], { icon: svgIcon }).addTo(map);
+      L.marker( [(res.features[0].geometry.coordinates.at(0)).at(1), (res.features[0].geometry.coordinates.at(0)).at(0)], { icon: svgIcon }).addTo(map);
+
+    });  
+  }
+
+  makeFinishIcon(map:any): void{
+    
+    this.http.get(this.path).subscribe((res: any) => {
+
+      console.log( "lt " + (res.features[0].geometry.coordinates.at(-1)).at(1));
+      console.log(( "long " + res.features[0].geometry.coordinates.at(-1)).at(0));
+   
+      
+      const svgIcon = L.divIcon({
+        html: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-3" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff4500" fill="#ff4500" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16" />
+</svg>`,
+        className: "",
+        iconSize: [24, 40],
+        iconAnchor: [12, 40],
+      }); 
+      
+      L.marker( [(res.features[0].geometry.coordinates.at(-1)).at(1), (res.features[0].geometry.coordinates.at(-1)).at(0)], { icon: svgIcon }).addTo(map);
+
+    });  
+  }
+
 
 }
